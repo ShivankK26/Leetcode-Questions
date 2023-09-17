60dc653b0ae35cf00b25b8100a6d9b9addf9b8c1
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2023-09-17 13:53:18 +0530

    Create Problem-367.java

---
+++ Problem-367.java
@@ -0,0 +1,43 @@
+/*
+
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+You must not use any built-in library function, such as sqrt.
+
+
+Example 1:
+
+Input: num = 16
+Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+
+
+Example 2:
+
+Input: num = 14
+Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+
+*/
+
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 1;
+        long end = num;
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+            long sq = mid * mid;
+            if(sq == num){
+                return true;
+            } else if(sq > num){
+                end = mid - 1;
+            } else {     
+                start = mid + 1;
+            }            
+        }
+        return false;
+    }
+}
